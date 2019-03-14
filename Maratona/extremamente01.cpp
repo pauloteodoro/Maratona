8af044256de3a86dbadcf56bdeaf348fda22e215
Paulo Teodoro <paulo.teodoro01@fatec.sp.gov.br>
2019-03-14 00:04:04 -0300

    dois primeiros

---
+++ Maratona/extremamente01.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int A=0,B=0,X=0;
+
+
+  cin >> A;
+  cin >> B;
+
+  cout << "X = "<< A + B << endl;
+
+  return 0; 
+}
---
+++ Maratona/soma.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+int A=0,B=0,SOMA=0;
+cin >> A;
+cin >> B;
+cout << "SOMA = " << A+B << endl;
+}
